4fad9ef6e53d7c4d173d597e0e0e70bdbefae20f
Srikanth Sharma <snow.650@gmail.com>
2020-04-11 04:54:22 +0530

    Android: Enable/Disable wi-fi

--- android/app/src/main/java/com/nativemodules/Counter.java
+++ android/app/src/main/java/com/nativemodules/Counter.java
@@ -5,14 +5,21 @@ import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import android.net.wifi.WifiManager;
+import android.content.Context;
 
 public class Counter extends ReactContextBaseJavaModule {
     private static Integer count = 0;
+    private static WifiManager wifiManager;
+    private static Boolean wifiState = true;
     public Counter(ReactApplicationContext reactContext) {
         super(reactContext);
+        wifiManager = (WifiManager) getReactApplicationContext().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
     }
     @ReactMethod public void increment() {
         count++;
+        wifiManager.setWifiEnabled(wifiState);
+        wifiState = !wifiState;
         System.out.println(count);
     }
     @ReactMethod public void getCount(Callback successCallback) {
